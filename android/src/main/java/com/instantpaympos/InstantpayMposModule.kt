2d1baf9ebfdbb6062969eb147497c2b3cad77fec
smithspeed <dhananjayspeed@gmail.com>
2023-01-24 01:20:48 +0530

    upds

--- android/src/main/java/com/instantpaympos/InstantpayMposModule.kt
+++ android/src/main/java/com/instantpaympos/InstantpayMposModule.kt
@@ -3,16 +3,12 @@ package com.instantpaympos
 import `in`.credopay.payment.sdk.CredopayPaymentConstants
 import `in`.credopay.payment.sdk.PaymentActivity
 import `in`.credopay.payment.sdk.PaymentManager
+import `in`.credopay.payment.sdk.Utils
 import android.app.Activity
 import android.content.Intent
 import android.util.Log
-import com.facebook.react.bridge.Arguments
-import com.facebook.react.bridge.BaseActivityEventListener
-import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.WritableMap
+import androidx.core.content.ContextCompat
+import com.facebook.react.bridge.*
 import org.json.JSONObject
 import org.json.JSONTokener
 
